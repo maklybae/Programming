141e8eda98a572a4b9ec33e803e51fc427e9cacb
Maksim Klychkov <maxim.klychkov64@gmail.com>
2023-09-10 18:56:26 +0300

    Task01 solve

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,9 @@
-﻿// Your code here!
+﻿using static System.Console;
+
+string? surname = ReadLine();
+string? name = ReadLine();
+string? patronymic = ReadLine();
+
+WriteLine($"Фамилия: {surname}");
+WriteLine($"Имя: {name}");
+WriteLine($"Отчество: {patronymic}");
