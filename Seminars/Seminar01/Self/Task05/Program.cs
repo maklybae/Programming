cca38907a26732cb3f00994f5e9c5d4ddcde89b1
Maksim Klychkov <maxim.klychkov64@gmail.com>
2023-09-11 17:42:25 +0300

    Task05 solve

--- Seminars/Seminar01/Self/Task05/Program.cs
+++ Seminars/Seminar01/Self/Task05/Program.cs
@@ -1,1 +1,7 @@
-﻿// Your code here!
+﻿using static System.Console;
+
+double.TryParse(ReadLine(), out double u);
+double.TryParse(ReadLine(), out double r);
+
+WriteLine($"I = {u / r}");
+WriteLine($"P = {u * u / r}");
