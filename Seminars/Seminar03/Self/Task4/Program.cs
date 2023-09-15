6012aec6ac3270aff5eb9f9932291349043ba034
Maksim Klychkov <maxim.klychkov64@gmail.com>
2023-09-15 19:43:32 +0300

    Task04 solve

---
+++ Seminars/Seminar03/Self/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+
+    static double F(double x, double y)
+    {
+        if (x < y)
+        {
+            return Math.Sin(x) + Math.Cos(y) * Math.Cos(y);
+        }
+        else if (x == y)
+        {
+            return Math.Log(Math.Abs(x));
+        }
+        else
+        {
+            return Math.Sin(x * x) + Math.Cos(y);
+        }
+    }
+    static void Main()
+    {
+        double.TryParse(Console.ReadLine(), out double x);
+        double.TryParse(Console.ReadLine(), out double y);
+        Console.WriteLine(F(x, y));
+    }
+}
+
