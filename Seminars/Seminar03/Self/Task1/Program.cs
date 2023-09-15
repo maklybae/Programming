27f30ab2317d3d3b4bdab883ae7f8af306cfc39b
Maksim Klychkov <maxim.klychkov64@gmail.com>
2023-09-15 19:01:31 +0300

    Task01 solve

---
+++ Seminars/Seminar03/Self/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+int.TryParse(Console.ReadLine(), out int n);
+double.TryParse(Console.ReadLine(), out double l);
+
+double s = (n * l * l) / (4 * Math.Tan(Math.PI / n));
+Console.WriteLine($"{s:f2}");
