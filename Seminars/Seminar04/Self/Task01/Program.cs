931fcd8b27eed3beb2fdc15e24f3c8164d39c9a3
Maksim Klychkov <maxim.klychkov64@gmail.com>
2023-09-20 23:03:02 +0300

    Task01 solve

---
+++ Seminars/Seminar04/Self/Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+char symbol = (char)Console.Read();
+switch (symbol)
+{
+    case char chr when char.IsDigit(chr):
+        Console.WriteLine(100);
+        break;
+    case char chr when char.IsUpper(chr):
+        Console.WriteLine(200);
+        break;
+    case char chr when char.IsLower(chr):
+        Console.WriteLine(300);
+        break;
+    default:
+        Console.WriteLine(400);
+        break;
+}
