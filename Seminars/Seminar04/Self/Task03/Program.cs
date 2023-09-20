e91a3fc429d5780e99c53e461f6f175f9b34eb52
Maksim Klychkov <maxim.klychkov64@gmail.com>
2023-09-20 23:03:33 +0300

    Task03 solve

---
+++ Seminars/Seminar04/Self/Task03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task03;
+class Program
+{
+    static void Main(string[] args)
+    {
+        if(!int.TryParse(Console.ReadLine(), out int command) || command < 1 ||  command > 4)
+        {
+            Console.WriteLine("Некорректный код операции");
+            return;
+        }
+        if(!int.TryParse(Console.ReadLine(), out int a))
+        {
+            Console.WriteLine("Некорректный тип данных");
+            return;
+        }
+        if (!int.TryParse(Console.ReadLine(), out int b))
+        {
+            Console.WriteLine("Некорректный тип данных");
+            return;
+        }
+        switch (command)
+        {
+            case 1:
+                Console.WriteLine($"{a} + {b} = {a + b}");
+                break;
+            case 2:
+                Console.WriteLine($"{a} - {b} = {a - b}");
+                break;
+            case 3:
+                Console.WriteLine($"{a} * {b} = {a * b}");
+                break;
+            case 4:
+                Console.WriteLine($"{a} / {b} = {a / b}");
+                break;
+        }
+    }
+}
